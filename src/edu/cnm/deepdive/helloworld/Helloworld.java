3f312c569108826c56572dde3ddb1181888a36de
fbeji <fbeji@unm.edu>
2019-01-03 17:21:34 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/helloworld/Helloworld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Simple class that displays the "Hello World" message.
+ *
+ */
+public class Helloworld {
+
+	/**
+	 * Prints "Hello World!" to standard output.
+	 * 
+	 * @param args    Command-line parameters (ignored).
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
